40f8fa9539486304a429d67bb624c6a272fccf35
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-29 20:47:34 +0000

    Update SecurityConfig.java
    access permitted to another pages

--- src/main/java/org/example/taskmanager/config/SecurityConfig.java
+++ src/main/java/org/example/taskmanager/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package org.example.taskmanager.config;
 
+import org.example.taskmanager.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,20 +14,34 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private CustomUserDetailsService userDetailsService; // Injeção do UserDetailsService
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/login", "/register").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/login", "/register").permitAll() // Permite acesso às páginas de login e registro
+                        .requestMatchers("/index.html", "/register.html", "/login.html").permitAll() //Permite acesso à página index
+                        .anyRequest().authenticated() //Todas as outras requisições devem estar autenticadas
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/tasks", true)
+                        .defaultSuccessUrl("/", true)
                 )
                 .logout(logout -> logout.logoutSuccessUrl("/login"));
 
         return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService);
+        return authenticationManagerBuilder.build();
     }
 }
 
