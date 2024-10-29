5cdf6e229be25dd5121b00660416a0c2036fbbf0
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-29 20:46:39 +0000

    Create MyTaskController.java

---
+++ src/main/java/org/example/taskmanager/controller/MyTaskController.java
@@ -0,0 +1,26 @@
+package org.example.taskmanager.controller;
+
+import org.example.taskmanager.model.MyTask;
+import org.example.taskmanager.service.MyTaskService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/my-task")
+public class MyTaskController {
+
+    private final MyTaskService myTaskService;
+
+    public MyTaskController(MyTaskService myTaskService) {
+        this.myTaskService = myTaskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MyTask> createTask(@RequestBody MyTask myTask) {
+
+        MyTask myNewTask = myTaskService.saveTask(myTask);
+        return ResponseEntity.status(HttpStatus.CREATED).body(myNewTask);
+    }
+}
