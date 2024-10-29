045ec1eb0e2a44185c6f43af9386951daea7774a
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-29 20:45:05 +0000

    Update UserRepository.java
    classe renamed

--- src/main/java/org/example/taskmanager/repository/UserRepository.java
+++ src/main/java/org/example/taskmanager/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package org.example.taskmanager.repository;
 
-import org.example.taskmanager.model.User;
+import org.example.taskmanager.model.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+public interface UserRepository extends JpaRepository<MyUser, Long> {
+    MyUser findByUsername(String username);
 }
