cab6ffffb59e8d4205e811a74a9f7bf31e683f36
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-29 20:46:37 +0000

    Create MyUserController.java

---
+++ src/main/java/org/example/taskmanager/controller/MyUserController.java
@@ -0,0 +1,28 @@
+package org.example.taskmanager.controller;
+
+import org.example.taskmanager.model.MyUser;
+import org.example.taskmanager.service.MyUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class MyUserController {
+
+    private final MyUserService myUserService;
+
+    public MyUserController(MyUserService myUserService) {
+        this.myUserService = myUserService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MyUser> createUser(@RequestBody MyUser myUser) {
+
+        MyUser newMyUser = myUserService.saveUser(myUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newMyUser);
+    }
+}
