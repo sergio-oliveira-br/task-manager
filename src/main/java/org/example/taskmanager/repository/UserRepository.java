574ba31a271d97276b264fe78205137eef49db99
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-29 16:10:05 +0000

    Create UserRepository.java
    Act:
    -> Repository implemented

---
+++ src/main/java/org/example/taskmanager/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.taskmanager.repository;
+
+import org.example.taskmanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
