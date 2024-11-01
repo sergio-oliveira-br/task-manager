9cf5823a12f0b26b7a8772da0df90886258cffdf
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-01 11:35:46 +0000

    Update SecurityConfig.java
    removing things that I can't handle to run my app

--- src/main/java/org/example/taskmanager/config/SecurityConfig.java
+++ src/main/java/org/example/taskmanager/config/SecurityConfig.java
@@ -1,47 +1,39 @@
 package org.example.taskmanager.config;
 
-import org.example.taskmanager.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+//Configure as regras de autenticação e autorização no projeto.
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private CustomUserDetailsService userDetailsService; // Injeção do UserDetailsService
+    private final CustomUserDetailsService userDetailsService;
 
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/login", "/register").permitAll() // Permite acesso às páginas de login e registro
-                        .requestMatchers("/index.html", "/register.html", "/login.html").permitAll() //Permite acesso à página index
-                        .anyRequest().authenticated() //Todas as outras requisições devem estar autenticadas
-                )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/", true)
-                )
-                .logout(logout -> logout.logoutSuccessUrl("/login"));
-
-        return http.build();
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
 
     @Bean
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService);
-        return authenticationManagerBuilder.build();
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
 
