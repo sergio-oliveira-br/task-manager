fc8c8ecbac264fe529f05b92b558e098a1570294
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-01 11:39:14 +0000

    Update AuthController.java
    removing things unnecessary or that I cannot handle yet

--- src/main/java/org/example/taskmanager/controller/AuthController.java
+++ src/main/java/org/example/taskmanager/controller/AuthController.java
@@ -1,17 +1,17 @@
-package org.example.taskmanager.controller;
-
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/login")
-public class AuthController {
-
-    @PostMapping
-    public String myTest() {
-
-        System.out.println("myTest");
-        return "test";
-    }
-
-}
+//package org.example.taskmanager.controller;
+//
+//
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/login")
+//public class AuthController {
+//
+//    @PostMapping
+//    public String myTest() {
+//
+//        System.out.println("myTest");
+//        return "test";
+//    }
+//
+//}
