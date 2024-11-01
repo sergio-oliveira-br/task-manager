ab6468f5a8487b73684ef0a8d82b0243132623d5
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-01 11:35:35 +0000

    Update AuthController.java
    removing things that I can't handle to run my app

--- src/main/java/org/example/taskmanager/controller/AuthController.java
+++ src/main/java/org/example/taskmanager/controller/AuthController.java
@@ -1,34 +1,17 @@
 package org.example.taskmanager.controller;
 
-import org.springframework.ui.Model;
-import org.example.taskmanager.model.MyUser;
-import org.example.taskmanager.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/register")
+@RequestMapping("/login")
 public class AuthController {
 
-    private final UserRepository userRepository;
-
-    @Autowired
-    public AuthController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @PostMapping
-    public String registerUser(@RequestBody MyUser myUser) {
+    public String myTest() {
 
-        myUser.setPassword("{noop}" + myUser.getPassword()); // Armazena a senha sem codificação
-        userRepository.save(myUser);
-        return "redirect:/login"; // Redireciona para a página de login após o registro
+        System.out.println("myTest");
+        return "test";
     }
 
-    @GetMapping
-    public String showRegistrationForm(Model model) {
-
-        model.addAttribute("myUser", new MyUser());
-        return "register"; // Retorna o nome da página de registro
-    }
 }
