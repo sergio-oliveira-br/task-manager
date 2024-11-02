b751382cc998a5846db0b86f8ac1fbd00bf081df
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-02 01:24:15 +0000

    Update MyTask.java
    this was one attempt to make it works

--- src/main/java/org/example/taskmanager/model/MyTask.java
+++ src/main/java/org/example/taskmanager/model/MyTask.java
@@ -3,63 +3,22 @@ package org.example.taskmanager.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 public class MyTask {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String title;
-    private String description;
-    private String status;
-    private LocalDateTime createdAt;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID TaskId;
+    private String taskTitle;
+    private String taskDescription;
+    private String taskStatus;
+    private LocalDateTime taskCreatedAt;
 
     //Vincula uma tarefa a um usuário, e permite que um usuário tenha várias tarefas.
     @ManyToOne
     private MyUser myUser;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 
     public MyUser getUser() {
         return myUser;
@@ -67,5 +26,46 @@ public class MyTask {
 
     public void setUser(MyUser myUser) {
         this.myUser = myUser;
+    }
+
+
+    public UUID getTaskId() {
+        return TaskId;
+    }
+
+    public void setTaskId(UUID taskId) {
+        TaskId = taskId;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public LocalDateTime getTaskCreatedAt() {
+        return taskCreatedAt;
+    }
+
+    public void setTaskCreatedAt(LocalDateTime taskCreatedAt) {
+        this.taskCreatedAt = taskCreatedAt;
     }
 }
