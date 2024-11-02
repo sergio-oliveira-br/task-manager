bed347f603b2c520f027a3cba560e99f337b6feb
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-02 01:25:21 +0000

    Update AuthController.java
    attempt

--- src/main/java/org/example/taskmanager/controller/AuthController.java
+++ src/main/java/org/example/taskmanager/controller/AuthController.java
@@ -1,17 +1,18 @@
-//package org.example.taskmanager.controller;
-//
-//
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/login")
-//public class AuthController {
-//
-//    @PostMapping
-//    public String myTest() {
-//
-//        System.out.println("myTest");
-//        return "test";
-//    }
-//
-//}
+package org.example.taskmanager.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping()
+public class AuthController {
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        System.out.println("Test endpoint was called!");
+        return ResponseEntity.ok("Test POSTMAN,  endpoint is working!");
+    }
+
+}
