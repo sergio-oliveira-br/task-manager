eeee8bf501eb155b624f751f5c56f1751fb04249
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-03 21:22:20 +0000

    Update LoginController.java
    This method recognizes the user but the security context is not set

--- src/main/java/org/example/taskmanager/controller/LoginController.java
+++ src/main/java/org/example/taskmanager/controller/LoginController.java
@@ -1,8 +1,9 @@
 package org.example.taskmanager.controller;
 
+import org.example.taskmanager.config.CustomUserDetailsService;
 import org.example.taskmanager.service.MyUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -12,32 +13,43 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/login")
 public class LoginController {
 
-    private final MyUserService myUserService;
+    //private final MyUserService myUserService;
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService customUserDetailsService;
 
-    @Autowired
-    public LoginController(MyUserService myUserService) {
-        this.myUserService = myUserService;
+
+    public LoginController(MyUserService myUserService, AuthenticationManager authenticationManager, CustomUserDetailsService customUserDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.customUserDetailsService = customUserDetailsService;
     }
 
 
-//    @PostMapping
-//    public String login(@RequestParam String username, @RequestParam String password) {
-//
-//        boolean isAuthtenticated = myUserService.userDataChecker(username, password);
-//        System.out.println("Boolean Status: " + isAuthtenticated);
-//
-//        // Logando o estado do SecurityContext
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        System.out.println("Authentication: " + authentication);
-//
-//        if (isAuthtenticated) {
-//            return "redirect:/index.html";
-//        }
-//        return "redirect:/login.html";
-//    }
+    @PostMapping
+    public String login(@RequestParam String username, @RequestParam String password) {
 
-    @GetMapping
-    public String showLoginForm() {
-        return "login"; // Retorna a página de login
+        System.out.println("Acessando o metodo Login..\n");
+        System.out.println("Tentando autenticar usuário: " + username);
+
+        // Cria um token de autenticação
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+        System.out.println("Criando Token:" +  authToken);
+
+        // Realiza a autenticação
+        Authentication authentication = authenticationManager.authenticate(authToken);
+        System.out.println("Realizando a autenticacao: " + authentication);
+
+        // Armazena a autenticação no contexto de segurança
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (authentication.isAuthenticated()) {
+            System.out.println("Usuário autenticado: " + authentication.getName());
+            System.out.println("Authorities: " + authentication.getAuthorities());
+
+
+            return "redirect:/index.html";
+            //return ResponseEntity.status(HttpStatus.ACCEPTED).body(authentication.getPrincipal());
+        }
+        System.out.println("Usuario nao autenticado: " + authentication.getName());
+        return "redirect:/login.html";
+        //return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(authentication.getPrincipal());
     }
 }
